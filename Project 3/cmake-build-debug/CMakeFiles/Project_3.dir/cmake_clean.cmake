18dc607ad78272f9c7569e9cdce618ef36a48c2d
syferfyre <tao.ling@ufl.edu>
2017-03-22 08:15:23 -0400

    init

---
+++ Project 3/cmake-build-debug/CMakeFiles/Project_3.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Project_3.dir/pa3.cpp.obj"
+  "Project_3.pdb"
+  "Project_3.exe"
+  "Project_3.exe.manifest"
+  "libProject_3.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Project_3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
