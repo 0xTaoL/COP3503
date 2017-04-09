385d67a93cb9735e837f031f85417ae6c9c0962c
syferfyre <tao.ling@ufl.edu>
2017-04-09 18:23:19 -0400

    init proj 3

--- Project 3/pa3.cpp
+++ Project 3/pa3.cpp
@@ -1,9 +1,33 @@
 #include <iostream>
+#include <fstream>
 #include <string>
 #include "pa3.h"
 
 using namespace std;
 
-int main(){
-        return 0;
+int main() {
+
+    bool validFile = false;
+    while(!validFile) {
+        try {
+            cout << "Please enter the name of the input file: ";
+            string input;
+            cin >> input;
+            ifstream code;
+            code.open(input);
+            validFile = true;
+        }
+        catch (exception) {
+            cout << "Invalid file name, please try again.";
+        }
+    }
+
+    cout << "The depth of the nested loop(s) is " + depth() + "\n\n";
+    cout << "Keywords: " + keywords() + "\n" +
+            "Identifier: " + identifier() + "\n" +
+            "Constant: " + constant() + "\n" +
+            "Operators: " + operators() + "\n" +
+            "Delimiter: " + delimiter() + "\n\n" +
+            "Syntax Error(s): " + errors();
+    return 0;
 }
